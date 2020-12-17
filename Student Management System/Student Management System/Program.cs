68fdcf6550976f9f815ed0fdf0834fc2cb219642
ismail5g <ismail27.dec@gmail.com>
2020-12-17 20:26:14 +0600

    Project Initial

---
+++ Student Management System/Student Management System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Student_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
